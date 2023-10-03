6239f0a67409bece7898b3b1a67033d9474e655a
KofoCole <kofodym07@yahoo.com>
2023-10-03 09:38:24 +0100

    Created js file and declared the array of symbols

---
+++ resources/js/RockPaperScissors_Game.js
@@ -0,0 +1,9 @@
+//A PLAYER AGAINST THE COMPUTER
+const prompt = require("prompt-sync")();
+
+let username = prompt("Enter your Username: ");
+
+let symbols = ["rock", "paper", "scissors"];
+let countUser = 0;
+let countComputer = 0;
+
